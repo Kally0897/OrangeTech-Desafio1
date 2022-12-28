39cf5a335b5d86f70a53dd50b900337cbc2329e1
Kalliandra Lima <kalliandralima@gmail.com>
2022-12-28 20:31:36 -0300

    Alterações de estrutura

--- Mini_projetos/src/edu/kalliandra/anatomiadasclasses/BoletimEstudantil.java
+++ Mini_projetos/src/edu/kalliandra/anatomiadasclasses/BoletimEstudantil.java
@@ -1,4 +1,4 @@
-package edu.kalliandra.anatomiadasclasses;
+
 
 public class BoletimEstudantil {
     public static void main(String[] args) {
--- Mini_projetos/src/edu/kalliandra/anatomiadasclasses/Main.java
+++ Mini_projetos/src/edu/kalliandra/anatomiadasclasses/Main.java
@@ -1,4 +1,4 @@
-package edu.kalliandra.anatomiadasclasses;
+
 package edu.kalliandra.anatomiadasclasses;public class Main {
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
