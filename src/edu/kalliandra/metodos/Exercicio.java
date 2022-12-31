a784944dfef24f500ad5f26767ca471c1bbc7b24
Kalliandra Lima <kalliandralima@gmail.com>
2022-12-31 15:09:02 -0300

    "Inclusão de regras para criação de métodos"

---
+++ src/edu/kalliandra/metodos/Exercicio.java
@@ -0,0 +1,12 @@
+package src.edu.kalliandra.metodos;
+
+/*
+Criação de exemplo de uma classe representando uma Smartv
+características: ligada (boolean), canaç (int) e volume(int)
+Nossa TV poderá ligar e desligar e assim mudar o estado ligada
+Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1
+Nossa TV poderá mudar de canal de 1 em 1 ou definindo o número correspondente
+* */
+
+public class Exercicio {
+}
---
+++ src/edu/kalliandra/metodos/MyClass.java
@@ -0,0 +1,21 @@
+package src.edu.kalliandra.metodos;
+
+public class MyClass {
+
+    public double somar(int n1, int n2){
+        n1 = 2;
+        n2 = 4;
+        // LÓGICA - Finalidade do método
+        //return ... ;
+        return 6;
+    }
+
+    public void imprimir (String texto) {
+        //LÓGICA
+
+    }
+
+    public double dividir (int dividendo, int divisor) throws Exception {}
+
+    private void metodoprivado() {} // Este método não pode ser visto pro outras classes
+}
