3023321d470d805366ee9fde6595b5e4c709dd4c
Kalliandra Lima <kalliandralima@gmail.com>
2023-01-10 14:14:59 -0300

    "Commit de métodos em Java atv Empréstimo e Mensagem"

--- Metodos/src/one/edu/Emprestimo.java
+++ Metodos/src/one/edu/Emprestimo.java
@@ -4,4 +4,35 @@ Calcule o valor final de um empréstimo, a partir do valor solicitado. Taxas e p
 Defina arbitrariamente as faixas que  influenciam nos valores*/
 
 public class Emprestimo {
+
+    public static int getDuasParcelas() {
+        return 2;
+    }
+
+    public static int getTresParcelas() {
+        return 3;
+    }
+
+    public static double getTaxaDuasParcelas() {
+        return 0.35;
+    }
+
+    public static double getTaxaTresParcelas() {
+        return 0.55;
+    }
+
+    public static void  calcular( double valor, int parcelas) {
+        if (parcelas == 2){
+            double valorFinal = valor + (valor * getDuasParcelas());
+
+            System.out.println("Valor final do empréstimo em duas parcelas é : R$ " + valorFinal);
+        } else if (parcelas == 3){
+            double valorFinal = valor + (valor * getTresParcelas());
+
+            System.out.println("Valor final do empréstimo em três parcelas é : R$ " + valorFinal);
+
+        } else {
+            System.out.println("Quantidade máxima de parcelas excedida");
+        }
+    }
 }
--- Metodos/src/one/edu/Main.java
+++ Metodos/src/one/edu/Main.java
@@ -2,6 +2,25 @@ package one.edu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Calculadora
+        System.out.println("Resultados do exercício de calculadora");
+        Calculadora.soma(3,5);  //Aqui estamo chamando um método diretamente de uma classe, isso só é possível pq a classse é pública
+        Calculadora.subtracao(7,6);
+        Calculadora.multiplicacao(100, 5.2);
+        Calculadora.divisao(56, 9.1);
+
+        //Mensagem
+        System.out.println("Resultados do exercício de mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+
+        //Empréstimo
+        System.out.println("Resultados do exercício de empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(7500, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(5800, 5);
     }
 }
