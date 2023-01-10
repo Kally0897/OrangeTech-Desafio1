ab74ef3a655f426e0fb2121521ff60f370af57d6
Kalliandra Lima <kalliandralima@gmail.com>
2023-01-10 14:16:26 -0300

    "Commit de métodos em Java anotações extras"

--- Metodos/src/one/edu/Main.java
+++ Metodos/src/one/edu/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         //Calculadora
         System.out.println("Resultados do exercício de calculadora");
-        Calculadora.soma(3,5);  //Aqui estamo chamando um método diretamente de uma classe, isso só é possível pq a classse é pública
+        Calculadora.soma(3,5);  //Aqui estamos chamando um método diretamente de uma classe, isso só é possível pq a classse é pública
         Calculadora.subtracao(7,6);
         Calculadora.multiplicacao(100, 5.2);
         Calculadora.divisao(56, 9.1);
@@ -19,7 +19,7 @@ public class Main {
 
         //Empréstimo
         System.out.println("Resultados do exercício de empréstimo");
-        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas()); // é comum dentro de um método, chamarmos outro método, em JS chamamos de callback
         Emprestimo.calcular(7500, Emprestimo.getTresParcelas());
         Emprestimo.calcular(5800, 5);
     }
