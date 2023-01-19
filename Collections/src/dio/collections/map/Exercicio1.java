ed9d9b8777f0145764e7372eb143cb59ccb01682
Kalliandra Lima <kalliandralima@gmail.com>
2023-01-19 10:40:21 -0300

    "Exercícios Map"

---
+++ Collections/src/dio/collections/map/Exercicio1.java
@@ -0,0 +1,14 @@
+package dio.collections.map;
+
+import java.util.*;
+
+/*
+Dada a população estimada de alguns estados do nordeste brasileiro, faça:
+ Estado = PE - população = 9.616.621
+ Estado = AL - população = 3.351.543
+ Estado = CE - população  = 9.187.103
+ Estado = RN - população = 3.534.265
+ */
+
+public class Exercicio1 {
+}
---
+++ Collections/src/dio/collections/map/Exercicio2.java
@@ -0,0 +1,10 @@
+package dio.collections.map;
+import java.util.*;
+
+/*
+Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene.
+Depois, mostre quantas vezes cada valor foi conseguido.
+ */
+
+public class Exercicio2 {
+}
